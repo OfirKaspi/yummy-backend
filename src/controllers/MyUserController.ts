9c821eebb414c98043a52a7b2cba8b03a82f05ce
Ofir Kaspi <kaspiofir@gmail.com>
2024-09-06 19:38:49 +0300

    F/GetUser/Created get user route and a get user controller

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,5 +1,19 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import User from "../models/user"
+
+const getCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const currentUser = await User.findOne({ _id: req.userId })
+        if (!currentUser) {
+            return res.status(404).json({ message: "User not found" })
+        }
+
+        res.json(currentUser)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Error getting user" })
+    }
+}
 
 const createCurrentUser = async (req: Request, res: Response) => {
     try {
@@ -46,6 +60,7 @@ const updateCurrentUser = async (req: Request, res: Response) => {
 
 
 export default {
+    getCurrentUser,
     createCurrentUser,
     updateCurrentUser,
 }
