ca4f304e899248d0805d9651457e25a5f56f8330
Ofir Kaspi <kaspiofir@gmail.com>
2024-10-03 19:30:54 +0300

    F/Stripe/Place an order in the db.

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import Stripe from "stripe"
 import Restaurant, { MenuItemType } from "../models/restaurant"
+import Order from "../models/order"
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string)
 const FRONTEND_URL = process.env.FRONTEND_URL as string
@@ -29,11 +30,20 @@ const createCheckoutSession = async (req: Request, res: Response) => {
             throw new Error("Restaurant not found")
         }
 
+        const newOrder = new Order({
+            restaurant: restaurant,
+            user: req.userId,
+            status: "placed",
+            deliveryDetails: checkoutSessionRequest.deliveryDetails,
+            cartItems: checkoutSessionRequest.cartItems,
+            createdAt: new Date()
+        })
+
         const lineItems = createLineItems(checkoutSessionRequest, restaurant.menuItems)
 
         const session = await createSession(
             lineItems,
-            "TEST_ORDER_ID",
+            newOrder._id.toString(),
             restaurant.deliveryPrice,
             restaurant._id.toString()
         )
@@ -42,8 +52,8 @@ const createCheckoutSession = async (req: Request, res: Response) => {
             return res.status(500).json({ message: "Error creating stripe session" })
         }
 
+        await newOrder.save()
         res.json({ url: session.url })
-
     } catch (error: any) {
         console.log("Error: ", error)
         res.status(500).json({ message: error.raw.message })
@@ -102,7 +112,7 @@ const createSession = async (
             restaurantId
         },
         success_url: `${FRONTEND_URL}/order-status?success=true`,
-        cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+        cancel_url: `${FRONTEND_URL}/details/${restaurantId}?cancelled=true`,
     })
     return sessionData
 }
